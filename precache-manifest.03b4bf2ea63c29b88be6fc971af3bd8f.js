501e890205fdea6314267b8e1f320ad85dbbf3cb
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-07 14:48:14 +0000

    Updates

---
+++ precache-manifest.03b4bf2ea63c29b88be6fc971af3bd8f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d8fff5d1a373fc6efceb021134a129",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cea48379feb2595f746e",
+    "url": "/static/css/main.1f7f5c10.chunk.css"
+  },
+  {
+    "revision": "e6431a508acb5a993822",
+    "url": "/static/js/2.3f6a43b7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3f6a43b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea48379feb2595f746e",
+    "url": "/static/js/main.914d2e74.chunk.js"
+  },
+  {
+    "revision": "7ae65d963f0863ee5a4e",
+    "url": "/static/js/runtime-main.6d089092.js"
+  },
+  {
+    "revision": "2ac3579eda91997a681b5a4719b97c56",
+    "url": "/static/media/man1.2ac3579e.jpg"
+  },
+  {
+    "revision": "99f12077965f824f7b061eb08aded55e",
+    "url": "/static/media/man2.99f12077.jpg"
+  },
+  {
+    "revision": "9a11d28d4eb01889cb7616495654a0fd",
+    "url": "/static/media/man3.9a11d28d.jpg"
+  },
+  {
+    "revision": "94b84b064235446dff348d36b0702104",
+    "url": "/static/media/pic1.94b84b06.jpg"
+  },
+  {
+    "revision": "f12762924aa5005d8a852606f9d1b73d",
+    "url": "/static/media/pic2.f1276292.jpg"
+  },
+  {
+    "revision": "85fd674322227952b2b2e9eeadb7a5bb",
+    "url": "/static/media/pic3.85fd6743.jpg"
+  }
+]);
